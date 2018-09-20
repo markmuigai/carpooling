44c39a72096b46a3a3539d6aef64f7a6c7bda04e
markmuigai <markmuigai@gmail.com>
2018-09-20 20:34:37 +0300

    Setup Ride and User relationship

---
+++ app/Ride.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ride extends Model
+{
+    //
+    public function User()
+    {
+      return $this->belongsTo(User::class);
+    }
+}
